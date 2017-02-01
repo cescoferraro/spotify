9a6e36d381a0f7f2462febf72a012308e5d013f8
cescoferraro <francescoaferraro@gmail.com>
2017-02-01 14:11:28 -0200

    remove spotify playlist component

--- src/components/dashboard/playlists.tsx
+++ src/components/dashboard/playlists.tsx
@@ -10,20 +10,16 @@ const size = {
 };
 const view = 'list'; // or 'coverart'
 const theme = 'black'; // or 'white'
-import SpotifyPlayer from 'react-spotify-player';
 
 export const PlaylistsComponent = ({playlists}) => {
     return (<div>
         <h3>Playlists:</h3>
         {playlists.map((playlist: Playlist) => {
-            console.log(playlist)
-            return (<SpotifyPlayer
-                key={Math.random()}
-                uri={playlist.uri}
-                size={size}
-                view={view}
-                theme={theme}
-            />)
+            return (<CardHeader key={Math.random()}
+                                title={playlist.id}
+                                subtitle={playlist.name}
+                                avatar={playlist.images[0].url}
+                                width="1"/>)
         })}
     </div>)
 };
