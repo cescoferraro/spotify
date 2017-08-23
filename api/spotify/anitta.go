bee19f89f8bc102800cc0a492eaa9712e45a3a1e
cescoferraro <francescoaferraro@gmail.com>
2017-08-23 15:12:20 -0300

    keep up to date

--- api/spotify/anitta.go
+++ api/spotify/anitta.go
@@ -2,6 +2,7 @@ package spotify
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/pkg/errors"
 	"github.com/zmb3/spotify"
@@ -18,15 +19,33 @@ func ShowFeelings(move bool, id string, token string) (*spotify.FullArtist, erro
 	if err != nil {
 		return Artist, errors.Wrap(err, "retrieveToken")
 	}
+	for _, tracklist := range IDS {
+		if move {
+			log.Println("+")
+			log.Println((len(tracklist)))
+			err = client.AddTracksToLibrary(tracklist...)
+			if err != nil {
+				return Artist, errors.Wrap(err, "retrieveToken")
+			}
+		} else {
+			log.Println("-")
+			log.Println((len(tracklist)))
+			err = client.RemoveTracksFromLibrary(tracklist...)
+			if err != nil {
+				return Artist, errors.Wrap(err, "retrieveToken")
+			}
+		}
+	}
+
 	if move {
-		err = client.AddTracksToLibrary(IDS...)
+		err = client.FollowArtist(spotify.ID(id))
 		if err != nil {
-			return Artist, errors.Wrap(err, "retrieveToken")
+			return Artist, errors.Wrap(err, "play error")
 		}
 	} else {
-		err = client.RemoveTracksFromLibrary(IDS...)
+		err = client.UnfollowArtist(spotify.ID(id))
 		if err != nil {
-			return Artist, errors.Wrap(err, "retrieveToken")
+			return Artist, errors.Wrap(err, "play error")
 		}
 	}
 	Artist, err = client.GetArtist(spotify.ID(id))
@@ -47,30 +66,38 @@ func Authss(token string) (spotify.Client, error) {
 }
 
 // ArtistTrack TODO: NEEDS COMMENT INFO
-func ArtistTrack(client spotify.Client, uri string) ([]spotify.ID, error) {
+func ArtistTrack(client spotify.Client, uri string) ([][]spotify.ID, error) {
 	var IDS []spotify.ID
 	albumObject, err := client.GetArtistAlbums(spotify.ID(uri))
 	if err != nil {
 		fmt.Printf("%s\n", err.Error())
-		return IDS, errors.Wrap(err, "Follow")
+		return split(IDS, 50), errors.Wrap(err, "Follow")
 	}
-	var Tracks []spotify.SimpleTrack
 	for _, albums := range albumObject.Albums {
 		trackPage, err := client.GetAlbumTracks(albums.ID)
 		if err != nil {
 			fmt.Printf("%s\n", err.Error())
-			return IDS, errors.Wrap(err, "Follow")
+			return split(IDS, 50), errors.Wrap(err, "Follow")
 		}
-		for _, track := range trackPage.Tracks {
-			Tracks = append(Tracks, track)
+		// 	for _, track := range trackPage.Tracks {
+		// 		Tracks = append(Tracks, track)
+		// 	}
+		// fmt.Println(Tracks)
+		for _, id := range trackPage.Tracks {
+			IDS = append(IDS, id.ID)
 		}
 	}
-	fmt.Println(Tracks)
-	for _, id := range Tracks {
-		IDS = append(IDS, id.ID)
+	return split(IDS, 50), nil
+}
+func split(buf []spotify.ID, lim int) [][]spotify.ID {
+	var chunk []spotify.ID
+	chunks := make([][]spotify.ID, 0, len(buf)/lim+1)
+	for len(buf) >= lim {
+		chunk, buf = buf[:lim], buf[lim:]
+		chunks = append(chunks, chunk)
 	}
-	if len(IDS) > 50 {
-		return IDS[:50], nil
+	if len(buf) > 0 {
+		chunks = append(chunks, buf[:len(buf)])
 	}
-	return IDS, nil
+	return chunks
 }
