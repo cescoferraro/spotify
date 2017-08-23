8b57e70b976f86bc38b127bdfdb04d9386c824f0
cescoferraro <francescoaferraro@gmail.com>
2017-08-23 18:12:08 -0300

    cool refactor for better internet usage

--- internal/scripts/buildGO.sh
+++ internal/scripts/buildGO.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-go build -o api/spotifyapi -ldflags "-X main.version=$VERSION" api/main.go
+go build -o api/dist/spotify -ldflags "-X main.version=$VERSION" api/main.go
+cp api/Dockerfile api/dist/Dockerfile
