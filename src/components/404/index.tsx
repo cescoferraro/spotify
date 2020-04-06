40f842b1bd6ff3d7aa06d0e9fc8e7be30c751b7d
cescoferraro <francescoaferraro@gmail.com>
2020-04-06 10:41:19 -0300

    remove all server side render stuff

--- src/components/404/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-export const NoMatch = ({location}) => {
-    return (
-        <div>
-            Nothing matched {location.pathname}.
-        </div>
-    );
-};
--- webpack.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = env => [
-  require('./webpack.client.js')(env),
-  require('./webpack.server.js')(env)
-];
