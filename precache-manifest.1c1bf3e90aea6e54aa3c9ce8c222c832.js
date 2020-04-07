19288f297c5dc50e8e0e27e95183c37ef599540f
cescoferraro <francescoaferraro@gmail.com>
2020-04-07 09:02:40 -0300

    Updates

---
+++ precache-manifest.1c1bf3e90aea6e54aa3c9ce8c222c832.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96a845ea96db66dc0d2870dac0823891",
+    "url": "/spotify/index.html"
+  },
+  {
+    "revision": "b1a3b5759cd4e0e6d98e",
+    "url": "/spotify/static/css/main.f5fb7f02.chunk.css"
+  },
+  {
+    "revision": "d9e10b305716b1ecf1dc",
+    "url": "/spotify/static/js/2.b670ac36.chunk.js"
+  },
+  {
+    "revision": "abf645e52aaec27b0c377a66bf00f797",
+    "url": "/spotify/static/js/2.b670ac36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1a3b5759cd4e0e6d98e",
+    "url": "/spotify/static/js/main.33053ef6.chunk.js"
+  },
+  {
+    "revision": "cd845c25ad159d041ac2",
+    "url": "/spotify/static/js/runtime-main.21992fbe.js"
+  },
+  {
+    "revision": "7254f0e13830b7adefee3a2b0e765a27",
+    "url": "/spotify/static/media/boil.7254f0e1.jpg"
+  }
+]);
