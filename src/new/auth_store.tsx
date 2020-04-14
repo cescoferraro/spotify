10b262bb14a30b818dd16687a19268497aa74800
cescoferraro <francescoaferraro@gmail.com>
2020-04-14 13:04:33 -0300

    fix travis

--- src/new/auth_store.tsx
+++ src/new/auth_store.tsx
@@ -3,39 +3,32 @@ import store from "store";
 
 const key = "spotify-mui-store";
 const autoSave = (_this: Auth) => {
-    let firstRun = true;
-    autorun(() => {
-        if (firstRun) {
-            const existingStore = store.get(key);
-            if (existingStore) {
-                set(_this, {...existingStore, page: 0, index: 0, query: ""});
-            }
-        }
-        const hey = toJS(_this);
-        store.set(key, {...hey, page: 0, index: 0, query: ""});
-    });
-    firstRun = false;
+  let firstRun = true;
+  autorun(() => {
+    if (firstRun) {
+      const existingStore = store.get(key);
+      if (existingStore) {
+        set(_this, {...existingStore, page: 0, index: 0, query: ""});
+      }
+    }
+    const hey = toJS(_this);
+    store.set(key, {...hey, page: 0, index: 0, query: ""});
+  });
+  firstRun = false;
 };
 
 export class Auth {
 
-    @observable
-    public token: string | "initial" = "initial";
-    @observable
-    public profile: SpotifyProfile.RootObject | null = null;
+  @observable
+  public token: string | "initial" = "initial";
 
-    constructor() {
-        autoSave(this);
-    }
+  constructor() {
+    autoSave(this);
+  }
 
-    @action
-    public setProfile(st: SpotifyProfile.RootObject) {
-        this.profile = st;
-    }
-
-    @action
-    public setToken(st: string) {
-        this.token = st;
-    }
+  @action
+  public setToken(st: string) {
+    this.token = st;
+  }
 
 }
