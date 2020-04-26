4c61e30ab93c4f230590536b03e52b82770794df
cescoferraro <francescoaferraro@gmail.com>
2020-04-25 22:10:14 -0300

    actuallly use the library

--- api/schema/query.go
+++ api/schema/query.go
@@ -2,41 +2,11 @@ package schema
 
 import (
 	"github.com/cescoferraro/spotify/api/ispotify"
+	"github.com/cescoferraro/spotify/api/tools"
 	"github.com/cescoferraro/structql"
 	"github.com/graphql-go/graphql"
 	"github.com/zmb3/spotify"
-	"time"
 )
-
-type Whatever struct {
-	Whatever string `json:"whatever"`
-}
-type LocalTest struct {
-	Cesco    string    `json:"cesco"`
-	Whatever *Whatever `json:"whatever"`
-}
-
-type Token struct {
-	Code string `json:"code"`
-
-	Max int `json:"max"`
-
-	Valid bool `json:"valid"`
-
-	Expiry time.Time `json:"expiry"`
-
-	Data Data `json:"data"`
-}
-
-type Data struct {
-	Name string `json:"name"`
-
-	Trys int `json:"trys"`
-
-	Valid bool `json:"valid"`
-
-	Expiry time.Time `json:"expiry"`
-}
 
 var Query = graphql.NewObject(graphql.ObjectConfig{
 	Name: "Query",
@@ -45,23 +15,6 @@ var Query = graphql.NewObject(graphql.ObjectConfig{
 			Type: graphql.String,
 			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
 				return ispotify.Auth().AuthURL("dashboard"), nil
-			},
-		},
-		"token": &graphql.Field{
-			Type: structql.GenerateType(Token{}),
-			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-				return Token{
-					Code:   "super-secret",
-					Max:    2,
-					Valid:  false,
-					Expiry: time.Now(),
-					Data: Data{
-						Name:   "yay",
-						Trys:   40,
-						Valid:  true,
-						Expiry: time.Now(),
-					},
-				}, nil
 			},
 		},
 		"test": &graphql.Field{
@@ -90,49 +43,12 @@ var Query = graphql.NewObject(graphql.ObjectConfig{
 				},
 			}),
 			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-				//token := p.Context.Value(tools.Key("token")).(string)
-				//user, err := ispotify.PlayerState(token)
-				//if err != nil {
-				//	return ispotify.EmptyResponseMeansNotListening(err, user)
-				//}
-				artists := []spotify.SimpleArtist{{}}
-				images := []spotify.Image{{}}
-				item := spotify.FullTrack{
-					SimpleTrack: spotify.SimpleTrack{
-						Artists:          artists,
-						AvailableMarkets: nil,
-						URI:              "",
-					},
-					Album: spotify.SimpleAlbum{
-						Name:   "sdfsdf",
-						Images: images,
-					},
-					ExternalIDs: nil,
-					Popularity:  3230,
+				token := p.Context.Value(tools.Key("token")).(string)
+				user, err := ispotify.PlayerState(token)
+				if err != nil {
+					return ispotify.EmptyResponseMeansNotListening(err, user)
 				}
-				return spotify.PlayerState{
-					CurrentlyPlaying: spotify.CurrentlyPlaying{
-						Timestamp: 0,
-						PlaybackContext: spotify.PlaybackContext{
-							Endpoint: "",
-							Type:     "",
-							URI:      "",
-						},
-						Progress: 0,
-						Playing:  false,
-						Item:     &item,
-					},
-					Device: spotify.PlayerDevice{
-						ID:         "",
-						Active:     false,
-						Restricted: false,
-						Name:       "dev",
-						Type:       "",
-						Volume:     0,
-					},
-					ShuffleState: false,
-					RepeatState:  "",
-				}, nil
+				return user, nil
 			},
 		},
 	},
