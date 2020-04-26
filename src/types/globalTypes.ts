7b79b943c2e861ea9c3c840ba993bd3a5c6ce36b
cescoferraro <francescoaferraro@gmail.com>
2020-04-26 01:26:59 -0300

    add LICENSE

---
+++ src/types/globalTypes.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+//==============================================================
+// START Enums and Input Objects
+//==============================================================
+
+//==============================================================
+// END Enums and Input Objects
+//==============================================================
+
+// @ts-ignore
+interface IGlobalTypes {
+  New: string
+}
